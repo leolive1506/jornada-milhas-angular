4b9fcb015a6b93642b7ed2835653adcb1c0966ae
Leonardo Lopes <leonardolivelopes2@gmail.com>
2024-01-23 15:53:14 -0300

    create modal

---
+++ src/app/shared/botao-controle/botao-controle.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-botao-controle',
+  templateUrl: './botao-controle.component.html',
+  styleUrls: ['./botao-controle.component.scss']
+})
+export class BotaoControleComponent {
+  @Input() operacao: 'incrementar' | 'decrementar' = "incrementar";
+}
